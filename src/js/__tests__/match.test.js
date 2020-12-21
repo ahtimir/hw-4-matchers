214342e92101eb22c5b3201d823388c77cf2eb24
ahtimir <ahtimir@gmail.com>
2020-12-21 22:24:42 +0300

    second commit

--- src/js/__tests__/match.test.js
+++ src/js/__tests__/match.test.js
@@ -55,17 +55,9 @@ test('match toEqual test (7 person squad)', () => {
   expect(sorted).toEqual(squadSorted);
 });
 
-test('match toBe test', () => {
-  const squad = [
-    { name: 'мечник', health: 10 },
-    { name: 'маг', health: 100 },
-    { name: 'лучник', health: 80 },
-  ];
-  const squadSorted = [
-    { name: 'маг', health: 100 },
-    { name: 'лучник', health: 80 },
-    { name: 'мечник', health: 10 },
-  ];
-  const sorted = sortHeroes(squad);
-  expect(sorted).not.toBe(squadSorted);
+test('squad error', () => {
+  const squad = undefined;
+  expect(() => {
+    sortHeroes(squad);
+  }).toThrow();
 });
--- src/js/match.js
+++ src/js/match.js
@@ -1,6 +1,9 @@
 /* eslint-disable linebreak-style */
 
 export default function sortHeroes(heroes) {
+  if (heroes === undefined || null) {
+    throw new Error('Ошибка отряда!');
+  }
   heroes.sort((a, b) => b.health - a.health);
   return heroes;
 }
